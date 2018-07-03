0670cdebe175a7110e4451e730353b7e9191b456
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-03 18:57:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,9 +10,8 @@ def oxford_comma(array)
     for i in 0..len-1
       if i == len-1
         temp_string += ", and " + array[i] 
-      else
-        temp_string += array[i] + ", "
       end
+      temp_string += array[i] + ", "
     end
     return temp_string
   end
