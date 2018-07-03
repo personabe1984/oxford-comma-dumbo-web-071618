abd4773f1e363e21046ca281ac1c94828fd69798
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-03 18:38:45 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-  array.join(", ")
+  len = array.length
+  if len == 1 
+    array.join(", ")
+  elsif len == 2
+    array.join(" and ")
+  elsif len == 3
+    array.join(", ")
+    
+  end
 end
