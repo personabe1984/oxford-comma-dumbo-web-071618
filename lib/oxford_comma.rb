6ac86548cecf1d1d1845085933164c8430fb6671
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-03 19:01:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
 def oxford_comma(array)
-
+  len = array.length
+  if len == 1 
+    array.join(", ")
+  elsif len == 2
+    array.join(" and ")
+  elsif len >= 3
+    # I could create a new array and then concatinate it 
+    temp_string = ""
+    for i in 0..len-1
+      if i == len-1
+        temp_string += "and " + array[i] 
+      else
+        temp_string += array[i] + ", "
+      end
+    end
+    return temp_string
+  end
 end
+
+#arr = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
+
+#puts oxford_comma(arr)
