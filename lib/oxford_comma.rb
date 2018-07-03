103d93f829b0cf96ff8a23e833e54b9381d6cf4f
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-03 18:52:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
     array.join(" and ")
   elsif len >= 3
     # I could create a new array and then concatinate it 
-    for i in array
+    for i in 
       puts i
     end
     
